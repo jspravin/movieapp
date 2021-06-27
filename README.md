a2f9ec5c7cef8e5deae45e6a6cb713883aabc616
Pravin Kumar J.Sukumaran <spravinkumar@athenahealth.com>
2021-06-27 21:24:19 +0530

    Assignemnt Movie App

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pashu Movie App
+
+Assignment for building a React Movie App using OMDB API
+
+# Project setup
+
+### `npm install`
+
+# Project start
+
+### `npm run start`
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import MovieAppProvider from "./context/MovieContext";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MovieAppProvider>
+      <App />
+    </MovieAppProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
